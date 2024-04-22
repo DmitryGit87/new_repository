5eb479a6c52d3fa5b6ddced5f94b84f281b02d4b
DmitryGit87 <149331750+DmitryGit87@users.noreply.github.com>
2024-04-22 22:48:57 +0300

    Commited1

--- src/JavaRush/FknCycles.java
+++ src/JavaRush/FknCycles.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class FknCycles {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int count = 0;
-        for(int i=0; i<10; i++){
+        int count = 1;
+        for(int i=1; i<10; i++){
         if(scanner.hasNextInt())
             count++;
             scanner.nextInt();
