c24de359e8ee081c62d08b243de2981958207eaa
DmitryGit87 <149331750+DmitryGit87@users.noreply.github.com>
2024-04-22 23:04:16 +0300

    My commit. In this version fix some bugs for you

--- src/JavaRush/FknCycles.java
+++ src/JavaRush/FknCycles.java
@@ -4,7 +4,7 @@ public class FknCycles {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int count = 1;
-        for(int i=1; i<10; i++){
+        for(int i=1; i<20; i++){
         if(scanner.hasNextInt())
             count++;
             scanner.nextInt();
